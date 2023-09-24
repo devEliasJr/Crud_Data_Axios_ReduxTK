c0aba7d0a1d186066cbcf612a8f32ace6b4d9e37
devEliasJr <eliasengmecanica@gmail.com>
2023-09-24 18:01:13 -0300

    feat: Services to getting data from API and handling errors

---
+++ src/Store/Services/userServices.ts
@@ -0,0 +1,69 @@
+import { AxiosError } from "axios";
+import { api } from "../../hooks/useapi";
+
+const getAllDataService = async () => {
+  try {
+    const response = await api.get<IDataProps[]>("/products");
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+export const getUserByIdService = async (id: number) => {
+  try {
+    const response = await api.get<IDataProps>(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+const createUserService = async (
+  userData: Pick<IDataProps, "name" | "url" | "description">
+) => {
+  try {
+    const response = await api.post<IDataProps>("/products", userData);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+const editUserService = async ({ userId, userData }: IDataEditProps) => {
+  try {
+    const response = await api.put(`/products/${userId}`, userData);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+const deleteUserService = async (userId: number) => {
+  try {
+    await api.delete(`/products/${userId}`);
+    return userId;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.message);
+    }
+  }
+};
+
+const userService = {
+  getAllDataService,
+  getUserByIdService,
+  createUserService,
+  editUserService,
+  deleteUserService,
+};
+
+export default userService;
